b385a3fc4da0728d8115ff94e043a37d3f1ef330
Kristian Azmanov <Kristian_Azmanov@epam.com>
2016-07-08 18:15:09 +0300

    Initial Commit

---
+++ MvcAppClient/MvcAppClient/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(MvcAppClient.Startup))]
+
+namespace MvcAppClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "TempCookie",
+                AuthenticationMode = AuthenticationMode.Passive
+            });
+        }
+    }
+}
